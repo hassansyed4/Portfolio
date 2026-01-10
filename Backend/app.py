527b1923de501089f41fd96a11ea5d260b50d4fa
SYED HASSAN AHMED <hassan.tonk@gmail.com>
2026-01-10 16:49:15 -0600

    Render deploy setup

--- Backend/app.py
+++ Backend/app.py
@@ -86,5 +86,6 @@ def serve_frontend(filename):
 # ---------- START SERVER ----------
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
